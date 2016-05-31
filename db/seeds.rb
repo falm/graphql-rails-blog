28750c6d939c9ce364051fb41a975a2180e95464
Jason Hou <hjj1992@gmail.com>
2016-05-31 09:40:25 +0800

    seeds data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,32 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+user_data = 3.times.map do
+  {name: Faker::Internet.user_name, email: Faker::Internet.email}
+end
+
+users = User.create(user_data)
+
+article_date = users.map do |user|
+
+  3.times.map do
+    {title: Faker::Name.title, content: Faker::Lorem.paragraph, user_id: user.id}
+  end
+
+end.flatten
+
+articles = Article.create(article_date)
+
+comment_data = articles.map do |article|
+
+  3.times.map do
+    {article_id: article.id, content: Faker::Lorem.paragraph, user_id: User.all.to_a.sample.id}
+  end
+
+end.flatten
+
+comments = Comment.create(comment_data)
+
+
